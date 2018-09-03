da8869937612f9d5cc55136ec9b15562e386651a
Nick Mitchell <nick@kitset.io>
2018-09-04 03:24:22 +1000

    Updated README and docs

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -57,18 +57,140 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         <a href="modules/AppModule.html" data-type="entity-link">AppModule</a>
                             <li class="chapter inner">
                                 <div class="simple menu-toggler" data-toggle="collapse"
-                                    ${ isNormalMode ? 'data-target="#components-links-module-AppModule-1158a6a8cf0cae7aa6834e2ca9e16a44"' : 'data-target="#xs-components-links-module-AppModule-1158a6a8cf0cae7aa6834e2ca9e16a44"' }>
+                                    ${ isNormalMode ? 'data-target="#components-links-module-AppModule-28f923b5e2055ffcc173838ddc0ef5a5"' : 'data-target="#xs-components-links-module-AppModule-28f923b5e2055ffcc173838ddc0ef5a5"' }>
                                     <span class="icon ion-md-cog"></span>
                                     <span>Components</span>
                                     <span class="icon ion-ios-arrow-down"></span>
                                 </div>
                                 <ul class="links collapse"
-                                    ${ isNormalMode ? 'id="components-links-module-AppModule-1158a6a8cf0cae7aa6834e2ca9e16a44"' : 'id="xs-components-links-module-AppModule-1158a6a8cf0cae7aa6834e2ca9e16a44"' }>
+                                    ${ isNormalMode ? 'id="components-links-module-AppModule-28f923b5e2055ffcc173838ddc0ef5a5"' : 'id="xs-components-links-module-AppModule-28f923b5e2055ffcc173838ddc0ef5a5"' }>
                                         <li class="link">
                                             <a href="components/AppComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">AppComponent</a>
                                         </li>
+                                        <li class="link">
+                                            <a href="components/ContainerComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">ContainerComponent</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="components/FlickrDetailsComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">FlickrDetailsComponent</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="components/FlickrFavouritesComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">FlickrFavouritesComponent</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="components/FlickrListComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">FlickrListComponent</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="components/FlickrSearchComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">FlickrSearchComponent</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="components/FlickrTileComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">FlickrTileComponent</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="components/LayoutHeaderComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">LayoutHeaderComponent</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="components/SidebarComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">SidebarComponent</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="components/SidebarItemComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">SidebarItemComponent</a>
+                                        </li>
                                 </ul>
                             </li>
+                            <li class="chapter inner">
+                                <div class="simple menu-toggler" data-toggle="collapse"
+                                    ${ isNormalMode ? 'data-target="#injectables-links-module-AppModule-28f923b5e2055ffcc173838ddc0ef5a5"' : 'data-target="#xs-injectables-links-module-AppModule-28f923b5e2055ffcc173838ddc0ef5a5"' }>
+                                    <span class="icon ion-md-arrow-round-down"></span>
+                                    <span>Injectables</span>
+                                    <span class="icon ion-ios-arrow-down"></span>
+                                </div>
+                                <ul class="links collapse"
+                                    ${ isNormalMode ? 'id="injectables-links-module-AppModule-28f923b5e2055ffcc173838ddc0ef5a5"' : 'id="xs-injectables-links-module-AppModule-28f923b5e2055ffcc173838ddc0ef5a5"' }>
+                                        <li class="link">
+                                            <a href="injectables/FavouritesFactory.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules"}>FavouritesFactory</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="injectables/FlickrApiService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules"}>FlickrApiService</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="injectables/PhotoCache.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules"}>PhotoCache</a>
+                                        </li>
+                                </ul>
+                            </li>
+                    </li>
+                    <li class="link">
+                        <a href="modules/AppRoutingModule.html" data-type="entity-link">AppRoutingModule</a>
+                    </li>
+            </ul>
+        </li>
+                <li class="chapter">
+                    <div class="simple menu-toggler" data-toggle="collapse"
+                    ${ isNormalMode ? 'data-target="#components-links"' : 'data-target="#xs-components-links"' }>
+                        <span class="icon ion-md-cog"></span>
+                        <span>Components</span>
+                        <span class="icon ion-ios-arrow-down"></span>
+                    </div>
+                    <ul class="links collapse"
+                    ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AuthComponent.html" data-type="entity-link">AuthComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ConfirmEmailComponent.html" data-type="entity-link">ConfirmEmailComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ForgotPasswordComponent.html" data-type="entity-link">ForgotPasswordComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LoginComponent.html" data-type="entity-link">LoginComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProfileComponent.html" data-type="entity-link">ProfileComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/RegisterComponent.html" data-type="entity-link">RegisterComponent</a>
+                            </li>
+                    </ul>
+                </li>
+        <li class="chapter">
+            <div class="simple menu-toggler" data-toggle="collapse"
+            ${ isNormalMode ? 'data-target="#classes-links"' : 'data-target="#xs-classes-links"' }>
+                <span class="icon ion-ios-paper"></span>
+                <span>Classes</span>
+                <span class="icon ion-ios-arrow-down"></span>
+            </div>
+            <ul class="links collapse"
+            ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                    <li class="link">
+                        <a href="classes/FlickrPhoto.html" data-type="entity-link">FlickrPhoto</a>
+                    </li>
+            </ul>
+        </li>
+        <li class="chapter">
+            <div class="simple menu-toggler" data-toggle="collapse"
+                ${ isNormalMode ? 'data-target="#interfaces-links"' : 'data-target="#xs-interfaces-links"' }>
+                <span class="icon ion-md-information-circle-outline"></span>
+                <span>Interfaces</span>
+                <span class="icon ion-ios-arrow-down"></span>
+            </div>
+            <ul class="links collapse"
+            ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                    <li class="link">
+                        <a href="interfaces/IFlickrApiContract.html" data-type="entity-link">IFlickrApiContract</a>
+                    </li>
+                    <li class="link">
+                        <a href="interfaces/IFlickrApiItemContract.html" data-type="entity-link">IFlickrApiItemContract</a>
+                    </li>
+                    <li class="link">
+                        <a href="interfaces/IFlickrFeedContract.html" data-type="entity-link">IFlickrFeedContract</a>
+                    </li>
+                    <li class="link">
+                        <a href="interfaces/PhotoHashTable.html" data-type="entity-link">PhotoHashTable</a>
+                    </li>
+                    <li class="link">
+                        <a href="interfaces/PhotoHashTable-1.html" data-type="entity-link">PhotoHashTable</a>
+                    </li>
+                    <li class="link">
+                        <a href="interfaces/SidebarItem.html" data-type="entity-link">SidebarItem</a>
                     </li>
             </ul>
         </li>
@@ -86,6 +208,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                     </li>
             </ul>
         </li>
+            <li class="chapter">
+                <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+            </li>
         <li class="chapter">
             <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
         </li>
