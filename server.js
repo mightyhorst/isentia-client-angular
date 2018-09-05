1267192128c6329dacb9c4f7792ae899c8dceb1a
Nick Mitchell <nick@kitset.io>
2018-09-05 11:42:32 +1000

    Added shortcut redirects for express. This should be replaced with proper server configuration

--- server.js
+++ server.js
@@ -18,8 +18,40 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+/**
+*
+* Angular App 
+* The Angular app is served as a static page from the build directory 
+*
+**/
 app.use(express.static(path.join(__dirname, 'dist/isentia-client-angular')));
 app.use('/', express.static(path.join(__dirname, 'dist/isentia-client-angular')));
+
+
+/**
+*
+* Angular App redirects
+* This is a shortcut to setting up the production server correctly
+* See here: https://angular.io/guide/deployment#production-servers
+*
+* For the sake of brevity, if the link is copy pasted or bookmarked we need to reduct them to the home page to load the angular app
+* After the angular app is loaded it will use Html5 paths to control the routes inside the SPA 
+*
+**/
+app.get('/photo', function(req, res) {
+	res.redirect('/');
+})
+app.get('/photo/:any', function(req, res) {
+	res.redirect('/');
+})
+
+
+/**
+*
+* Express API 
+* This is an alternative to the API Gateway that is left here as an example, but is not used by the Angular App
+*
+**/
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
